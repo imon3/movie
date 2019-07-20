8b302a216929f5ae213e509f6e73ba4dd7a59828
Imon Ovbude <imonovbude@gmail.com>
2019-07-19 19:37:07 -0700

    created logic to request movies from third party api

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -2,14 +2,54 @@ import React from "react";
 import "./App.css";
 import Nav from "./components/navbar/Nav";
 import SearchArea from "./components/searchBox/SearchArea";
+import MovieList from "./components/movies/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <SearchArea />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      searchTerm: ""
+    };
+
+    this.apiKey = process.env.REACT_APP_API;
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${
+        this.state.searchTerm
+      }`
+    )
+      .then(data => data.json())
+      .then(data => {
+        this.setState({
+          movies: [...data.results]
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleChange = e => {
+    this.setState({
+      searchTerm: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <SearchArea
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
+        <MovieList movies={this.state.movies} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ movie-search/src/components/movies/MovieList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Movie from "./Movie";
+
+const MovieList = props => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s12">
+          {props.movies.map((movie, index) => {
+            return <Movie key={index} image={movie.poster_path} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
