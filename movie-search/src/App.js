8cec953907c728a745b7bf0b0deeec7e8798f2ed
Imon Ovbude <imonovbude@gmail.com>
2019-07-22 10:41:26 -0700

    created method to view movie details and return to the main page from movie details

--- movie-search/src/App.js
+++ movie-search/src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/navbar/Nav";
 import SearchArea from "./components/searchBox/SearchArea";
 import MovieList from "./components/movies/MovieList";
 import Pagination from "./components/pagination/Pagination";
+import MovieInfo from "./components/movies/MovieInfo";
 
 class App extends React.Component {
   constructor() {
@@ -13,12 +14,14 @@ class App extends React.Component {
       movies: [],
       searchTerm: "",
       totalResults: 0,
-      currentPage: 1
+      currentPage: 1,
+      currentMovie: null
     };
 
     this.apiKey = process.env.REACT_APP_API;
   }
 
+  // Handle Submit method
   handleSubmit = e => {
     e.preventDefault();
     fetch(
@@ -37,12 +40,14 @@ class App extends React.Component {
       .catch(err => console.log(err));
   };
 
+  // Handle Change Method
   handleChange = e => {
     this.setState({
       searchTerm: e.target.value
     });
   };
 
+  // Go To Next Page Method
   nextPage = pageNumber => {
     fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${
@@ -59,17 +64,43 @@ class App extends React.Component {
       .catch(err => console.log(err));
   };
 
+  // View Clicked Movie Info
+  viewMovieInfo = id => {
+    const filteredMovie = this.state.movies.filter(movie => movie.id === id);
+    if (filteredMovie.length > 0) {
+      this.setState({
+        currentMovie: filteredMovie
+      });
+    }
+  };
+
+  closeMovieInfo = () => {
+    this.setState({
+      currentMovie: null
+    });
+  };
+
   render() {
     const numberPages = Math.floor(this.state.totalResults / 20);
 
     return (
       <div className="App">
         <Nav />
-        <SearchArea
-          handleSubmit={this.handleSubmit}
-          handleChange={this.handleChange}
-        />
-        <MovieList movies={this.state.movies} />
+        {this.state.currentMovie === null ? (
+          <div>
+            <SearchArea
+              handleSubmit={this.handleSubmit}
+              handleChange={this.handleChange}
+            />
+            <MovieList
+              movies={this.state.movies}
+              viewMovieInfo={this.viewMovieInfo}
+            />
+          </div>
+        ) : (
+          <MovieInfo closeMovieInfo={this.closeMovieInfo} />
+        )}
+
         {this.state.totalResults > 20 ? (
           <Pagination
             pages={numberPages}
